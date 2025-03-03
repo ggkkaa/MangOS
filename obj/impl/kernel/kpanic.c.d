94a3cc700d3e0b7a0928a6dab7eff423966df751
Luka <luka.talevski.hn@gmail.com>
2025-03-03 08:15:11 +0100

    Remove build files

--- obj/impl/kernel/kpanic.c.d
+++
@@ -1,7 +0,0 @@
-obj/impl/kernel/kpanic.c.o: src/impl/kernel/kpanic.c \
- src/impl/kernel/modules/utilities/include/print.h \
- src/impl/kernel/panic.h \
- src/impl/kernel/modules/utilities/include/utils.h
-src/impl/kernel/modules/utilities/include/print.h:
-src/impl/kernel/panic.h:
-src/impl/kernel/modules/utilities/include/utils.h:
