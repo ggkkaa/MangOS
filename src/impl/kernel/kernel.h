38c0e44e977ec646563636d0d147c216aacebbc3
Luka <luka.talevski.hn@gmail.com>
2025-03-04 12:26:13 +0100

    Plan out memory

--- src/impl/kernel/kernel.h
+++ src/impl/kernel/kernel.h
@@ -2,6 +2,16 @@
 #include "../x86_64/include/idt.h"
 #include "./memory/linked_list.h"
 
+#define REGION_USER_STACK_ADDR  0x0000700000000000LL
+#define REGION_USER_STACK_PAGES 6
+#define REGION_HHDM_ADDR  0xFFFF800000000000LL
+#define REGION_HHDM_PAGES 8388608
+#define REGION_IO_SPACE_ADDR  0xFFFF800800000000LL
+#define REGION_IO_SPACE_PAGES 524288
+#define REGION_KERNEL_STACK_ADDR  0xFFFFFFFFFFFF5FFFLL
+#define REGION_KERNEL_STACK_PAGES 10
+
+
 extern char kernel_start;
 extern char kernel_end;
 
